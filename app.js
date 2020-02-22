9e8acfba8432b9d40bd3fe8a7972298938a77204
AngryReiser <Halk001000@mail.ru>
2020-02-22 10:29:46 +0300

    SC

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
+    res.sendFile(__dirname + '/index.html');
+  });
 
 http.listen(8000, function(){
   console.log('listening on :8000');
 });
+
+io.on('connection', function(socket){
+    console.log('user connected: ' + socket.id);
+  });
